89cac777bd482c521c9cf8090f823eb27f026dee
Sawjal <mdsajalsikder@gmail.com>
2024-03-12 05:44:18 +0600

    if elif

--- practies.py
+++ practies.py
@@ -1,9 +1,30 @@
-# Given an integer, , perform the following conditional actions:
+# a = 30
+# b = 300
+# if a > b:
+#     print("a is greater than b")
+# elif a == b:
+#     print("a and b are equall")
+# else:
+#     print("b is greater than a")
 
-# If  is odd, print Weird
-# If  is even and in the inclusive range of  to , print Not Weird
-# If  is even and in the inclusive range of  to , print Weird
-# If  is even and greater than , print Not Weird
+# print("a is greater than b") if a > b else print("b is greater than a")
 
-x = input()
-print(x)
+
+# marks = 6
+
+# if marks >= 80 and 100 > marks:
+#     print("A+")
+# if marks >= 70 and 79 > marks:
+#     print ("A")
+# if marks >= 60 and 69 > marks:
+#     print("A-")
+# else:
+#     print("Failed")
+
+
+
+
+
+# a = 2520
+# b = 2520
+# print("a is greater than b") if a > b else print("a is equal") if a == b else print("a is less than b") 
