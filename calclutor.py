c05287cc5f3e4a8a000bada74c15de355e7125bd
Sawjal <mdsajalsikder@gmail.com>
2024-03-12 14:51:48 +0600

    calcluator

---
+++ calclutor.py
@@ -0,0 +1,14 @@
+a = int(input("Enter 1st number"))
+b = input("Enter Oparator")
+c = int(input("Enter 2nd number"))
+
+if b == "+":
+    print(a + c)
+elif b == "-":
+    print(a - c)
+elif b == "*":
+    print(a * c)
+elif b == "/":
+    print(a / c)
+else:
+    print("Error")
