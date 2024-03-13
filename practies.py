8f0c8f225bcbecce5515b2e1e5e7f1152f698faa
Sawjal <mdsajalsikder@gmail.com>
2024-03-14 02:19:26 +0600

    Problem Solve

--- practies.py
+++ practies.py
@@ -1,5 +1,5 @@
 
-# Passenger = input()
+# Passenger = input("Choice")
 
 # if Passenger == "Comilla":
 #     print("500 Taka")
@@ -10,11 +10,35 @@
 # else:
 #     print("Error")  
 
-service_year = input()
-Salary = input()
-persent = Salary * 0.05
 
-if service_year >= 5:
-    print({persent})
-else:
-    print("Error")
+# call = int(input("Select Your Satation"))
+
+# if call == 1:
+#     print("Give 500 taka")
+# elif call == 2:
+#     print("Give 1000 Taka")
+# elif call == 3:
+#     print("Give 1200 Taka")
+# else:
+#     print("error")
+
+
+# salary = int(input("Enter your Salary"))
+# Service_Year = float(input("Enter your Servicve Year"))
+# Bonus = salary*0.05
+
+# if Service_Year > 5:
+#     print(Bonus)
+# else:
+#     print("Out of Bonus")
+
+
+# x = str(input("Enter Your Fruits :"))
+# fruits = ["apple", "Orange", "Banana"]
+
+# report = "Yes, {}  is a fruits"
+
+# if x in fruits:
+#     print(report.format(x))
+
+
---
+++ problem_solve.py
@@ -0,0 +1,88 @@
+# Q8. Write a program to calculate the electricity bill (accept number of unit from user) according to the following criteria:
+
+# 1. First 100 units 	No Charge
+# 2. Next 100 unites 	Rs 5 per unit
+# 3. after 200 units 	Rs 10 per unit
+# (for example if input unit is 350 than total bill amount is Rs 2000)
+
+# unit = int(input("Enter unit"))
+# bill = 0
+
+# if unit <= 100:
+#     print("No Charge")
+# elif unit > 100 and unit <=200:
+#     print((unit-100)*5)
+# elif unit > 200:
+#     print(500+(unit-200)*10)
+# else:
+#     print("Error")
+
+
+
+# part 2
+# Q1.Write a program to accept percentage from the user and display the grade according to the following criteria:
+
+# marks			    Grade
+# >90			    A
+# >80 and <=90		B
+# >=60 and <=80		c
+# below 60		    d
+
+# marks = int(input("Enter Marks :"))
+
+# if marks > 90:
+#     print("Grade is : A")
+# elif marks > 80 and marks <= 90:
+#     print("Grade is : B")
+# elif marks >= 60 and marks <= 80:
+#     print("Grade is : C")
+# elif marks < 60 :
+#     print("Grade is : D")
+# else:
+#     print("Failed")
+
+
+# Q2.Write a program to accept the cost price of a bike and display the road tax to be paid according to the following criteria:
+
+# cost price (in Rs)	Tax
+# >100000			    15%
+# >50000 and <=100000	10%
+# <=50000			    5%
+
+# price = float(input("Enter your Bike price : "))
+
+# if price > 100000:
+#     print(price*0.15)
+# elif price > 50000 and price<= 100000:
+#     print(price*0.10)
+# elif price < 50000 :
+#     print(price*0.05)
+# else:
+#     print("error")
+
+
+
+
+
+# Q4. Write a program to accept a number from 1 to 7 and display the name of the day like 1 for sunday. 
+# 2 for monday and so on.
+
+# number = int(input("Enter a number from 1 to 7 : "))
+
+# if number == 1:
+#     print("Sunday")
+# elif number == 2:
+#     print("Monday")
+# elif number == 3:
+#     print("Tuesday")
+# elif number == 4:
+#     print("Wednesday")
+# elif number == 5:
+#     print("Thusday")
+# elif number == 6:
+#     print("Friday")
+# elif number == 7:
+#     print("Saturday")
+# else:
+#     print("Error")
+
