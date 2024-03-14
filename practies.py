8a98e5b1d9aae39790861dbf20ca0b960a1c682c
Md Sawjal Sikder <mdsajalsikder@gmail.com>
2024-03-14 14:27:01 +0600

    String Practies

--- practies.py
+++ practies.py
@@ -1,44 +1,18 @@
+# Strings are Arrays
+x = "Hello world"
+# print(x[3])
+# print(x[2:5])
+# print(len(x))
 
-# Passenger = input("Choice")
+# y = str(input("Search : "))
+# print(y in x)
+# if y in x:
+#     print("Yes")
 
-# if Passenger == "Comilla":
-#     print("500 Taka")
-# elif Passenger == "Chittagong":
-#     print(" Taka")
-# elif Passenger == "Rajshahi":
-#     print("1200 Taka")
-# else:
-#     print("Error")  
+# print(x[2:6])
+# print(x[:6])
+# print(x[6:])
+# print(x[-7:-2])
 
-
-# call = int(input("Select Your Satation"))
-
-# if call == 1:
-#     print("Give 500 taka")
-# elif call == 2:
-#     print("Give 1000 Taka")
-# elif call == 3:
-#     print("Give 1200 Taka")
-# else:
-#     print("error")
-
-
-# salary = int(input("Enter your Salary"))
-# Service_Year = float(input("Enter your Servicve Year"))
-# Bonus = salary*0.05
-
-# if Service_Year > 5:
-#     print(Bonus)
-# else:
-#     print("Out of Bonus")
-
-
-# x = str(input("Enter Your Fruits :"))
-# fruits = ["apple", "Orange", "Banana"]
-
-# report = "Yes, {}  is a fruits"
-
-# if x in fruits:
-#     print(report.format(x))
-
-
+# print(x.replace("H", "J"))
+# print(x.split(","))
