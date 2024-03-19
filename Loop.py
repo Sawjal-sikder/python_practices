eb4fb53d9d556b5a8deaf474b3862c10ae3b87d0
Md Sawjal Sikder <mdsajalsikder@gmail.com>
2024-03-19 15:23:00 +0600

    python Loop

---
+++ Loop.py
@@ -0,0 +1,75 @@
+# while loop
+# break Statement
+# continue Statement
+# else Statement
+
+# For Loops
+# fruits = ["apple", "banana", "cherry"]
+# Looping Through a String
+# break Statement
+# continue Statement
+# range() Function
+# Else in For Loop
+# Nested Loops
+# pass Statement
+
+
+
+# while loop
+number = 1
+# while number < 10:
+#     print(number)
+#     number += 1
+
+
+# break Statement
+# while number < 5:
+#     if number == 3:
+#         break
+#     print(number)
+#     number += 1
+
+# continue Statement
+# while number < 6:
+#     number += 1
+#     if number == 3:
+#         continue
+#     print(number)
+# else Statement
+# while number < 5:
+#     print(number)
+#     number += 1
+# else:
+#     print("No less than 6")
+
+
+
+
+# For Loops
+fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+
+# Looping Through a String
+# for x in "Apple":
+#     print(x)
+
+# break Statement
+# for x in fruits:
+#     print(x)
+#     if x == "banana":
+#         break
+
+# continue Statement
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+
+# range() Function
+# Else in For Loop
+# Nested Loops
+# pass Statement
+
+
+
