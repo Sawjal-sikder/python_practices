fe6bd6fbc4982e32bdecb71ab1d5683acf9c3c99
Md Sawjal Sikder <mdsajalsikder@gmail.com>
2024-03-24 23:30:28 +0600

    Row Python Practies

--- practies.py
+++ practies.py
@@ -1,41 +1,47 @@
+# Variable
+MyName = "md sawjal Sikder"
+print(MyName)
+# swap
+a = 5
+b = 10
+print(a,b)
+a,b = b,a
+print(a,b)
+# Multiple Variable
+a,b,c = "Md", "Sawjal", "sikder"
+print(a)
 
-# a = 1
-# while a <= 10:
-#     print("1 x",a,"=", a*1)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("2 x",a,"=", a*2)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("3 x",a,"=", a*3)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("4 x",a,"=", a*4)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("5 x",a,"=", a*5)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("6 x",a,"=", a*6)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("7 x",a,"=", a*7)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("8 x",a,"=", a*8)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("9 x",a,"=", a*9)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("10 x",a,"=", a*10)
-#     a+=1
+# One value to multiple variable
+a=b=c = "Md Sawjal Sikder"
+print(c)
+
+# Unpack collection
+fruits = "Apple", "Orange", "Banana"
+a,b,c = fruits
+print(a)
+
+# f string
+Name = "Md Sawjal Sikder"
+Age = 30
+print(f"Hello, My name is {Name} and age is {Age} years old.")
+
+# Data type 
+print(type(Name))
+print(float(Age))
+
+# Multiline String
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+print(Name[3:9])
+
+# Looping Through a String
+for x in Name:
+    print(x)
+print(len(Name))
+print("Md" in Name)
+
+if "Md" in Name:
+    print(f"Yes, Md are availavle in {Name}")
