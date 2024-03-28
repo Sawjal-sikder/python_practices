bdfd289d3596b365e5c2b344e9d2ca0e970076d2
Md Sawjal Sikder <mdsajalsikder@gmail.com>
2024-03-28 23:31:58 +0600

    loop practiess

---
+++ Loop_Excersie.py
@@ -0,0 +1,64 @@
+# Exercise 2: Print the following pattern
+
+# for i in range(1,6,1):
+#     for n in range(1,i+1,1):
+#         print(n, end="  ")
+#     print("")
+
+# Exercise 3: Calculate the sum of all numbers from 1 to a given number
+# sums = 0
+# for i in range(1,11,1):
+#     # print(i)
+#     sums += i
+# print(f"Total number is : {sums}")
+
+
+# Exercise 4: Write a program to print multiplication table of a given number
+# number = int(input("Enter your Number : "))
+# for i in  range(2,number+1,2):
+#     print(i)
+    
+
+# Exercise 5: Display numbers from a list using loop
+# numbers = [12, 75, 150, 180, 145, 525, 50]
+# for x in numbers:
+#     if x > 500:
+#         break
+#     elif x > 150:
+#         continue
+#     elif x%5 == 0:
+#         print(x)
+
+
+
+# Exercise 7: Print the following pattern
+# for x in range(1,6,1):
+#     for y in range(1,5+1,1):
+#         print('#', end=" ")
+#     print( " ")
+
+
+# for x in range(1,6):
+#     for y in range(6-x,0,-1):
+#         print(y, end=" ")
+#     print( " ")
+
+
+# Exercise 8: Print list in reverse order using a loop
+
+# list1 = [10, 20, 30, 40, 50]
+
+# for x in reversed(list1):    
+#     print(x)
+
+
+# Exercise 9: Display numbers from -10 to -1 using for loop
+
+# for i in range(-10,0,1):
+#     print(i)
+
+# Exercise 10: Use else block to display a message “Done” after successful execution of for loop
+# for i in range(5):
+#     print(i)
+# else:
+#     print("Done!") 
--- practies.py
+++ practies.py
@@ -1,173 +1,19 @@
-# # Variable
-# MyName = "md sawjal Sikder"
-# print(MyName)
-# # swap
-# a = 5
-# b = 10
-# print(a,b)
-# a,b = b,a
-# print(a,b)
-# # Multiple Variable
-# a,b,c = "Md", "Sawjal", "sikder"
-# print(a)
+a = [['naim' , 50] , ['sadi' , 12] , ['Rafi' , 2] , ['Ritu' , 80] , ['Basi' , 10]]    
 
-# # One value to multiple variable
-# a=b=c = "Md Sawjal Sikder"
-# print(c)
+x = []
+y = []
+output = []
 
-# # Unpack collection
-# fruits = "Apple", "Orange", "Banana"
-# a,b,c = fruits
-# print(a)
+for i in a:
+    x.append(i[1])
+x.sort()
+print(x)
 
-# # f string
-# Name = "Md Sawjal Sikder"
-# Age = 30
-# print(f"Hello, My name is {Name} and age is {Age} years old.")
-
-# # Data type 
-# print(type(Name))
-# print(float(Age))
-
-# # Multiline String
-# a = """Lorem ipsum dolor sit amet,
-# consectetur adipiscing elit,
-# sed do eiusmod tempor incididunt
-# ut labore et dolore magna aliqua."""
-# print(a)
-# print(Name[3:9])
-
-# # Looping Through a String
-# for x in Name:
-#     print(x)
-# print(len(Name))
-# print("Md" in Name)
-
-# if "Md" in Name:
-#     print(f"Yes, Md are availavle in {Name}")
-
-# print(Name[::-1])
-# print(Name.upper())
-# print(Name.lower())
-# print(Name.replace("Sawjal", "Sajal"))
-# print(Name.split(" "))
-
-# a = "Hello"
-# b = "World"
-# c = a+b
-# print(c)
-# print(a," ", b)
-# print(f"{a} {b}")
-
-# print(Name.capitalize())
-# c = Name.count("Md Sawjal Sikder")
-# print(c)
-
-# List in python
-# mylist = ["apple", "banana", "cherry"]
-# listi = ("apple", "banana", "cherry")
-# print(mylist)
-# print(len(mylist))
-# print(type(mylist))
-# print(list(listi))
-
-# print(mylist[2])
-# print(mylist[:2])
-# print(mylist[1:])
-# print(mylist[::-1])
-
-# if "apple" in mylist:
-#     print(f"{mylist[0]} in the list")
-
-# mylist[1] = "Orange"
-# print(mylist)
-
-# mylist[1:3] = ["blackcurrant", "watermelon"]
-# print(mylist)
-
-# mylist.insert(2, "Orange")
-# print(mylist)
-
-# mylist.append("Banana")
-# print(mylist)
-
-# tropical = ["mango", "pineapple", "papaya"]
-# mylist.extend(tropical)
-# print(mylist)
-
-# mylist.remove(["Banana"])
-# print(mylist)
-
-# mylist.pop(1)
-# print(mylist)
-
-# mylist.pop()
-# print(mylist)
-
-# del mylist[-1]
-# print(mylist)
-
-
-# i = int(input("Enter your Nunnber : "))
-
-# while i <= 10:
-#     print(i)
-#     i += 1
-
-# while i < 10:
-#     print(i)
-#     i += 1
-#     if i == 5:
-#         break
-
-# while i < 10:
-#     i += 1
-#     if i == 3:
-#         continue
-#     print(i)
-
-# while i < 10:
-#     print(i)
-#     i += 1
-# else:
-#     print("The End")
-
-
-# while i < 10:
-#     print(i)
-#     i += 1 
-#     if i == 4:
-#         break
-
-
-# while i < 10:
-#     i += 1
-#     if i == 5:
-#         continue
-#     print(i)
-# else:
-#     print("End the while loop")
-
-fruits = ["apple", "banana", "cherry"]
-
-# for x in fruits:
-#     print(x)
-
-
-# for x in "Md Sawjal Sikder":
-#     print(x)
-
-# for x in fruits:
-#     print(x)
-#     if x == "banana":
-#         break
-
-
-# for x in fruits:
-#     if x == "banana":
-#         continue
-#     print(x)
-
-
-# for x in range(1,11,1):
-#     print(f"5 x {x} = {x*5}")
+for i in a:
+    for j in x[:2]:
+        if j == i[1]:
+            y.append(i)
+print(y)
+for i in y:
+    output.append(i[0])
+print(output)
