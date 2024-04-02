4f5c1f5696cbd6d26b2cc2730a44a51d6593ab10
Md Sawjal Sikder <mdsajalsikder@gmail.com>
2024-04-02 15:59:01 +0600

    Problem Given

--- calclutor.py
+++ calclutor.py
@@ -1,14 +1,23 @@
-a = int(input("Enter 1st number"))
-b = input("Enter Oparator")
-c = int(input("Enter 2nd number"))
+# a = int(input("Enter 1st number"))
+# b = input("Enter Oparator")
+# c = int(input("Enter 2nd number"))
 
-if b == "+":
-    print(a + c)
-elif b == "-":
-    print(a - c)
-elif b == "*":
-    print(a * c)
-elif b == "/":
-    print(a / c)
-else:
-    print("Error")  
+# if b == "+":
+#     print(a + c)
+# elif b == "-":
+#     print(a - c)
+# elif b == "*":
+#     print(a * c)
+# elif b == "/":
+#     print(a / c)
+# else:
+#     print("Error")  
+
+
+
+
+a = [['naim' , 50] , ['sadi' , 12] , ['Rafi' , 48] , ['Ritu' , 80] , ['Basi' , 10]]
+for x in a:
+    for y in range(len(x)):
+        # y[0].sort()
+        print(y)
--- practies.py
+++ practies.py
@@ -1,41 +1,101 @@
+# 1. Write a Python program to schedule the tasks in such a way that the total penalty for missing deadlines is minimized. Your program should output the optimal order of tasks and the total penalty incurred.
+# Input:
+# A list of tasks, where each task is represented as a tuple (deadline, penalty). The deadline is an integer representing the number of days until the task must be completed, and the penalty is an integer representing the penalty for missing the deadline.
+# Output:
+# The optimal order of tasks to minimize the total penalty.
+# The total penalty incurred.
+# Example:
+# Input:
+# tasks = [(3, 100), (1, 300), (2, 200)]
+# Output:
+# Optimal order of tasks: [(1, 300), (2, 200), (3, 100)]
+# Total penalty incurred: 600
+# Note: In this example, completing the tasks in the order (1, 300), (2, 200), (3, 100) incurs a total penalty of 600, which is the minimum penalty possible for the given tasks.
+    
 
-# a = 1
-# while a <= 10:
-#     print("1 x",a,"=", a*1)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("2 x",a,"=", a*2)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("3 x",a,"=", a*3)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("4 x",a,"=", a*4)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("5 x",a,"=", a*5)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("6 x",a,"=", a*6)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("7 x",a,"=", a*7)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("8 x",a,"=", a*8)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("9 x",a,"=", a*9)
-#     a+=1
-# a = 1
-# while a <= 10:
-#     print("10 x",a,"=", a*10)
-#     a+=1
+
+
+
+
+# 2. Given an integer x, return true if x is a palindrome, and false otherwise.
+
+
+
+
+# 3. Write a function to find the longest common prefix string amongst an array of strings.
+# If there is no common prefix, return an empty string "".
+ 
+# Example 1:Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+
+# Example 2:Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+
+
+
+
+# 4.Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ 
+# Example 1:Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+# Example 2:Input: n = 1
+# Output: ["()"]
+
+ 
+# Constraints:
+# 1 <= n <= 8
+
+
+
+# 5.Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+# '?' Matches any single character.
+# '*' Matches any sequence of characters (including the empty sequence).
+# The matching should cover the entire input string (not partial).
+ 
+# Example 1:Input: s = "aa", p = "a"
+# Output: false
+# Explanation: "a" does not match the entire string "aa".
+
+# Example 2:Input: s = "aa", p = "*"
+# Output: true
+# Explanation: '*' matches any sequence.
+
+# Example 3:Input: s = "cb", p = "?a"
+# Output: false
+# Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+
+ 
+# Constraints:
+# 0 <= s.length, p.length <= 2000
+# s contains only lowercase English letters.
+# p contains only lowercase English letters, '?' or '*'.
+
+
+
+
+# 6.3The set [1, 2, 3, ..., n] contains a total of n! unique permutations.
+# By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
+# "123"
+# "132"
+# "213"
+# "231"
+# "312"
+# "321"
+# Given n and k, return the kth permutation sequence.
+ 
+# Example 1:Input: n = 3, k = 3
+# Output: "213"
+
+# Example 2:Input: n = 4, k = 9
+# Output: "2314"
+
+# Example 3:Input: n = 3, k = 1
+# Output: "123"
+
+ 
+# Constraints:
+# 1 <= n <= 9
+# 1 <= k <= n!
